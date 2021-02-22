102be3a753cadd0de18f5ecd6fce6585d27eea46
Allan Hipolito <allanhipolito66@gmail.com>
2021-02-22 14:50:56 -0300

    Desafio Terminado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request{
+  title:string,
+  value:number,
+  type: "income" | "outcome";
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,37 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title,value,type}:Request): Transaction {
+      const {total}= this.transactionsRepository.getBalance();
+
+      if(type === "outcome" && total < value){
+        throw new Error("You do not have enough balance.")
+      }
+      //INCLUDE
+      /*
+
+      if(!["income","outcome"].includes(type)){
+        throw new Error('Transaction type is invalid')
+      }
+      */
+
+     if(!["income","outcome"].includes(type)){
+      throw new Error('Transaction type is invalid')
+    }
+
+      //Minha lógica
+      /*if(type!="outcome" && type!="income"){
+        throw new Error("Your type of balance is not outcome or income. Please make your requisition again...")
+      }
+      */
+    
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    })
+
+    return transaction;
   }
 }
 
